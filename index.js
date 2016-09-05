525a9cffef63a25b9ffe93be307ace9ace9973c9
Michaël Zasso <targos@protonmail.com>
2016-09-05 13:40:15 +0200

    feat: add strict rule

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ module.exports = {
         'space-in-parens': ['error', 'never'],
         'space-infix-ops': 'error',
         'space-unary-ops': 'error',
+        'strict': ['error', 'global'],
         'no-only-tests/no-only-tests': 'error'
     }
 };
