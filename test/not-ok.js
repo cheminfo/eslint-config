272378b26a9b64e2d2e6e2cdd75272c43237ca28
Michaël Zasso <targos@protonmail.com>
2018-01-18 09:40:09 +0100

    feat: switch indent rule to two spaces (#7)

--- test/not-ok.js
+++ test/not-ok.js
@@ -8,14 +8,14 @@ var x;
 useIt(x);
 
 x = {
-    'useless': 'quotes'
+  'useless': 'quotes'
 };
 
 var y = 1, z;
 useIt(y, z);
 
 function A() {
-    this.x = 1;
+  this.x = 1;
 }
 
 new A();
@@ -26,7 +26,7 @@ new A();
 
 // use this function to mark a variable as used
 function useIt(...vals) {
-    return vals;
+  return vals;
 }
 
 
