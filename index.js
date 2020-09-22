15b1e49199b9e64d2778e21665c6410922a0794b
Michaël Zasso <targos@protonmail.com>
2020-09-22 09:15:08 +0200

    feat: enable no-loss-of-precision rule

--- index.js
+++ index.js
@@ -101,6 +101,7 @@ module.exports = {
     'no-inner-declarations': 'error',
     'no-invalid-regexp': 'error',
     'no-irregular-whitespace': 'error',
+    'no-loss-of-precision': 'error',
     'no-misleading-character-class': 'error',
     'no-obj-calls': 'error',
     'no-prototype-builtins': 'error',
