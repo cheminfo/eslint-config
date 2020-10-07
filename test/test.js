3b6d3b42e5c085e36d151d289f88226a8e6c2edf
Michaël Zasso <targos@protonmail.com>
2020-10-07 14:31:38 +0200

    chore: refactor test

--- test/test.js
+++ test/test.js
@@ -2,47 +2,51 @@
 
 const assert = require('assert');
 const util = require('util');
-const { CLIEngine } = require('eslint');
+const { ESLint } = require('eslint');
 
-const engine = new CLIEngine({ configFile: 'index.js' });
+const eslint = new ESLint({ overrideConfigFile: 'index.js' });
 
-const [okResult, notOkResult, jestResult] = engine.executeOnFiles([
-  'test/ok.js',
-  'test/not-ok.js',
-  'test/jest.test.js',
-]).results;
+eslint
+  .lintFiles(['test/ok.js', 'test/not-ok.js', 'test/jest.test.js'])
+  .then((results) => {
+    const [okResult, notOkResult, jestResult] = results;
 
-assert.strictEqual(
-  okResult.errorCount,
-  0,
-  'ok.js should have no error: ' + util.format(okResult),
-);
+    assert.strictEqual(
+      okResult.errorCount,
+      0,
+      'ok.js should have no error: ' + util.format(okResult),
+    );
 
-const errors = notOkResult.messages
-  .filter(isError)
-  .map((error) => error.ruleId)
-  .sort();
-assert.deepStrictEqual(errors, [
-  'no-console',
-  'no-redeclare',
-  'no-unused-vars',
-  'no-unused-vars',
-  'no-var',
-  'no-var',
-  'one-var',
-  'one-var-declaration-per-line',
-  'prettier/prettier',
-  'prettier/prettier',
-  'prettier/prettier',
-  'prettier/prettier',
-  'prettier/prettier',
-  'prettier/prettier',
-  'prettier/prettier',
-  'strict',
-  'wrap-iife',
-]);
+    const errors = notOkResult.messages
+      .filter(isError)
+      .map((error) => error.ruleId)
+      .sort();
+    assert.deepStrictEqual(errors, [
+      'no-console',
+      'no-redeclare',
+      'no-unused-vars',
+      'no-unused-vars',
+      'no-var',
+      'no-var',
+      'one-var',
+      'one-var-declaration-per-line',
+      'prettier/prettier',
+      'prettier/prettier',
+      'prettier/prettier',
+      'prettier/prettier',
+      'prettier/prettier',
+      'prettier/prettier',
+      'prettier/prettier',
+      'strict',
+      'wrap-iife',
+    ]);
 
-assert.strictEqual(jestResult.errorCount, 1);
+    assert.strictEqual(jestResult.errorCount, 1);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
 
 function isError(message) {
   return message.severity === 2;
