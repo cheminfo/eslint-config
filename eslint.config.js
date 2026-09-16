468c799b540ba85fdc7cd4ad0bf01f108c2dc347
Michaël Zasso <targos@protonmail.com>
2026-09-16 11:57:04 +0200

    feat: update unicorn plugin to v75

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,9 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 
 import cheminfo from './configs/cheminfo.js';
 
-const config = [cheminfo];
-
-if (!process.env.LINT_NOT_OK) {
-  config.push(globalIgnores(['test/not-ok.js', 'test/not_ok.test.js']));
-}
-
-export default defineConfig(config);
+export default defineConfig([
+  cheminfo,
+  ...(!process.env.LINT_NOT_OK
+    ? [globalIgnores(['test/not-ok.js', 'test/not_ok.test.js'])]
+    : []),
+]);
