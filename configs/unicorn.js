bf259b42f415f728e026b52e98046f399175f3dd
Michaël Zasso <targos@protonmail.com>
2026-09-24 12:39:53 +0200

    chore: alphabetize unicorn rules

--- configs/unicorn.js
+++ configs/unicorn.js
@@ -9,64 +9,64 @@ export default defineConfig([
     name: 'cheminfo/unicorn/rules',
     rules: {
       'unicorn/consistent-destructuring': 'error',
-      'unicorn/prefer-import-meta-properties': 'error',
+      // We don't use the feature.
+      'unicorn/expiring-todo-comments': 'off',
       // We are not consistent enough to enable this rule.
       'unicorn/filename-case': 'off',
       // Some prefer to always use destructuring.
       'unicorn/import-style': 'off',
-      // TODO: create issue about risk (>32bit integers)
-      'unicorn/prefer-math-trunc': 'off',
-      // We prefer Array.from(x) over [...x].
-      'unicorn/prefer-spread': 'off',
-      // We prefer new Array(x).fill() over Array.from({ length: x }).
-      'unicorn/no-new-array': 'off',
-      // We don't like separators after the comma.
-      'unicorn/numeric-separators-style': 'off',
-      // For loop is used sometimes for performance.
-      'unicorn/no-for-loop': 'off',
-      // We use null in many places.
-      'unicorn/no-null': 'off',
-      'unicorn/switch-case-braces': 'off',
-      'unicorn/prefer-global-this': 'off',
-      'unicorn/prefer-ternary': 'off',
-      'unicorn/prefer-type-error': 'off',
-      // Might want to enable in a future semver-major.
-      'unicorn/no-negated-condition': 'off',
-      'unicorn/no-useless-undefined': 'off',
-      // Conflicts with Prettier.
-      'unicorn/number-literal-case': 'off',
-      // Too early for full ESM.
-      'unicorn/prefer-module': 'off',
-      // Conflicts with Prettier.
-      'unicorn/no-nested-ternary': 'off',
-      // Problematic with useOnOff.
-      'unicorn/no-unreadable-array-destructuring': 'off',
-      'unicorn/prefer-regexp-test': 'off',
-      // Unfortunately, too annoying on valid cases, plus it conflicts with TS.
-      'unicorn/no-array-callback-reference': 'off',
-      // May conflict with other rule + ugly switch(0) auto-fix.
-      'unicorn/prefer-switch': 'off',
-      // Rare and problematic with APIs that have a find method.
-      'unicorn/no-array-method-this-argument': 'off',
-      // We don't use the feature.
-      'unicorn/expiring-todo-comments': 'off',
-      // Doesn't work with typescript-eslint v6 at the moment.
-      'unicorn/no-empty-file': 'off',
-      // We already have a rule for anonymous functions and tooling often does this with config.
-      'unicorn/no-anonymous-default-export': 'off',
       'unicorn/name-replacements': [
         'error',
         {
           allowList: nameReplacementsAllowList,
         },
       ],
-      // This is usually not a source of perf issues and maybe not the best heuristic.
-      'unicorn/prefer-simple-condition-first': 'off',
+      // We already have a rule for anonymous functions and tooling often does this with config.
+      'unicorn/no-anonymous-default-export': 'off',
+      // Unfortunately, too annoying on valid cases, plus it conflicts with TS.
+      'unicorn/no-array-callback-reference': 'off',
+      // Rare and problematic with APIs that have a find method.
+      'unicorn/no-array-method-this-argument': 'off',
       // Data processing algorithms are often based on nested loops and would be difficult to refactor for little benefit.
       'unicorn/no-break-in-nested-loop': 'off',
       // This is risky because it suggests to use Object.hasOwn, which will not behave the same when the existence check should ignore
       // existing properties with the value `undefined`.
       'unicorn/no-computed-property-existence-check': 'off',
+      // Doesn't work with typescript-eslint v6 at the moment.
+      'unicorn/no-empty-file': 'off',
+      // For loop is used sometimes for performance.
+      'unicorn/no-for-loop': 'off',
+      // Might want to enable in a future semver-major.
+      'unicorn/no-negated-condition': 'off',
+      // Conflicts with Prettier.
+      'unicorn/no-nested-ternary': 'off',
+      // We prefer new Array(x).fill() over Array.from({ length: x }).
+      'unicorn/no-new-array': 'off',
+      // We use null in many places.
+      'unicorn/no-null': 'off',
+      // Problematic with useOnOff.
+      'unicorn/no-unreadable-array-destructuring': 'off',
+      'unicorn/no-useless-undefined': 'off',
+      // Conflicts with Prettier.
+      'unicorn/number-literal-case': 'off',
+      // We don't like separators after the comma.
+      'unicorn/numeric-separators-style': 'off',
+      'unicorn/prefer-global-this': 'off',
+      'unicorn/prefer-import-meta-properties': 'error',
+      // TODO: create issue about risk (>32bit integers)
+      'unicorn/prefer-math-trunc': 'off',
+      // Too early for full ESM.
+      'unicorn/prefer-module': 'off',
+      'unicorn/prefer-regexp-test': 'off',
+      // This is usually not a source of perf issues and maybe not the best heuristic.
+      'unicorn/prefer-simple-condition-first': 'off',
+      // We prefer Array.from(x) over [...x].
+      'unicorn/prefer-spread': 'off',
+      // May conflict with other rule + ugly switch(0) auto-fix.
+      'unicorn/prefer-switch': 'off',
+      'unicorn/prefer-ternary': 'off',
+      'unicorn/prefer-type-error': 'off',
+      'unicorn/switch-case-braces': 'off',
     },
   },
 ]);
